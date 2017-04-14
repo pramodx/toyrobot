6eb27614425bf8f732e5dbfb7020166fece9fdcd
Pramod Nair <pramod.nair@emprevo.com>
2017-04-14 17:28:16 +1000

    finalising

--- js/script.js
+++ js/script.js
@@ -1,92 +1,105 @@
 let ToyRobotUtils = ((dcoument, window) => {
 
     /* Initial events binding */
+    let commandsObj = [];
+    let placed = false;
     let bindEvents = () => {
         let commandBtn = document.getElementById('formButton');
 
         commandBtn.addEventListener('click', function(e) {
-            // console.log(commands);
-            // console.log(commands.value);
+            ['error', 'report'].forEach(div => {
+                let node = document.getElementById(div);
+                while (node.hasChildNodes()) {
+                    node.removeChild(node.firstChild);
+                }
+            })
+            report(commands.value.split(/\r?\n/));
         }, false);
     }
 
+    /* set up object to manage direction states */
     let directions = {
-        'north': { val: 1, axis: 'y' },
-        'south': { val: -1, axis: 'y' },
-        'west': { val: -1, axis: 'x' },
-        'east': { val: 1, axis: 'x' }
+        'north': { key: 'north', left: 'west', right: 'east' },
+        'south': { key: 'south', left: 'east', right: 'west' },
+        'west': { key: 'west', left: 'south', right: 'north' },
+        'east': { key: 'east', left: 'north', right: 'south' },
     };
 
+    /* set up object to manage state of toy robot positions */
     let state = {
         x: 0,
         y: 0,
-        d: directions['north'].val,
+        d: directions['north'].key,
         axis: directions['north'].axis,
         get getState() {
             return {
                 x: this.x,
                 y: this.y,
-                d: this.d,
-                axis: this.axis
+                d: this.d
             }
         },
         set setState(obj) {
-            console.log(obj);
             this.x = obj.x;
             this.y = obj.y;
-            this.d = directions[obj.d].val;
-            this.axis = directions[obj.d].axis;
+            this.d = directions[obj.d].key;
         }
     };
 
+    /* get current state */
     let getState = () => {
         return state.getState
     }
 
+    /* set or update state  */
     let setState = (x, y, d) => {
         state.setState = { x: x, y: y, d: d }
     }
 
+    /* On move. No change of direction */
     let move = () => {
-        let newObj = getState();
-        let currentDirection;
-        for (let key in directions) {
-            if (directions.hasOwnProperty(key)) {
-                if (directions[key].val == newObj.d && directions[key].axis == newObj.axis) {
-                    currentDirection = key;
-                }
+        console.log(placed);
+        if (placed) {
+            let newObj = getState();
+            let currentDirection = newObj.d;
+
+            switch (currentDirection) {
+                case 'north':
+                    ++newObj.y;
+                    break;
+                case 'south':
+                    --newObj.y;
+                    break;
+                case 'east':
+                    ++newObj.x;
+                    break;
+                case 'west':
+                    --newObj.x;
+                    break;
+                default:
+            }
+            //console.log(newObj);
+            setState(newObj.x, newObj.y, currentDirection)
+
+
+            try {
+                checkPositionValidity(newObj.x, newObj.y);
+            } catch (error) {
+                handleError(new Error('Error'))
             }
         }
+    }
 
-        switch (currentDirection) {
-            case 'north':
-                ++newObj.y;
-                break;
-            case 'south':
-                --newObj.y;
-                break;
-            case 'east':
-                ++newObj.x;
-                break;
-            case 'west':
-                --newObj.x;
-                break;
-            default:
-        }
-        //console.log(newObj);
-        setState(newObj.x, newObj.y, currentDirection)
-
-        try {
-            checkPositionValidity(newObj.x, newObj.y);
-        } catch (error) {
-            handleError(error)
+    /* On Turn. No change of position. only direction  */
+    let turn = (turndirection) => {
+        if (placed) {
+            let newObj = getState();
+            let currentDirection = newObj.d;
+            let newDirection = directions[currentDirection][turndirection];
+            setState(newObj.x, newObj.y, newDirection);
         }
     }
 
-    let turn = () => {
-
-    }
-
+    /* validate position */
     let checkPositionValidity = (x, y) => {
         if ((x < 0) || (y < 0) || (x >= 5) || (y >= 5)) {
             throw new Error('Item out of bounds');
@@ -95,20 +108,78 @@ let ToyRobotUtils = ((dcoument, window) => {
         }
     }
 
+    /* placement */
     let place = (x, y, direction) => {
         try {
-            checkPositionValidity(x, y);
-            setState(x, y, d)
+            if (checkPositionValidity(x, y)) {
+                placed = true;
+                setState(x, y, direction)
+            }
+
         } catch (error) {
             handleError(error)
         }
     }
 
-    let handleError = (err) => {
-        let errorBlock = document.getElementById('error').createElement('span');
-        errorBlock.innerText = err;
+    /* reporting */
+    let generateReport = () => {
+        try {
+            let state = getState();
+            if (placed && checkPositionValidity(state.x, state.y)) {
+                let string = []
+                for (let key in state) {
+                    if (state.hasOwnProperty(key)) {
+                        string.push(state[key]);
+                    }
+                }
+                let reportBlock = document.getElementById('report');
+                let span = document.createElement('span');
+                let t = document.createTextNode(string.join(', ').toUpperCase());
+                span.appendChild(t);
+                reportBlock.appendChild(span);
+                placed = false;
+            }
+        } catch (error) {
+            handleError(new Error('Invalid result'))
+        }
     }
 
+    let report = (commands) => {
+        if (commands) {
+            commands.forEach(command => {
+                command = command.toLowerCase();
+                if (command.startsWith('place')) {
+                    command = command.replace('place', '');
+                    command = command.replace(/\s/g, '');
+                    let params = command.split(',');
+                    place(+params[0], +params[1], params[2].replace(/["']/g, ''));
+                } else if (command.startsWith('move')) {
+                    move();
+                } else if (command.startsWith('left')) {
+                    turn('left')
+                } else if (command.startsWith('right')) {
+                    turn('right')
+                } else if (command.startsWith('report')) {
+                    generateReport();
+                } else {
+                    handleError(new Error('Invalid Input'));
+                }
+            })
+        }
+
+    }
+
+    /* error handling */
+    let handleError = (err) => {
+        let errorBlock = document.getElementById('error');
+        //errorBlock.innerText = err;
+        let span = document.createElement('span');
+        let t = document.createTextNode(err);
+        span.appendChild(t);
+        errorBlock.appendChild(span);
+    }
+
+    /* bootstrap and bind events */
     let init = () => {
         bindEvents()
     }
@@ -119,6 +190,8 @@ let ToyRobotUtils = ((dcoument, window) => {
         getState: getState,
         setState: setState,
         move: move,
+        turn: turn,
+        report: report,
         checkPositionValidity: checkPositionValidity
     }
 
--- specs/spec.js
+++ specs/spec.js
@@ -6,35 +6,30 @@ describe('ToyRobotTests', function() {
 
     describe('1.When toy robot is placed', () => {
 
+        it('should correctly place robot if position x and y are between 0 and 4', () => {
+            expect(toyRobotUtils.checkPositionValidity(0, 3, 'north')).toBeTruthy();
+        });
+
         it('should throw error if toy topples off table', () => {
             expect(() => {
                 toyRobotUtils.place(0, 5, 'east')
             }).toThrowError(Error);
         });
 
-        it('should correctly place robot if position x and y are between 0 and 4', () => {
-            expect(toyRobotUtils.checkPositionValidity(0, 3, 'north')).toBeTruthy();
-        });
-
         it('should return updated current state', () => {
             toyRobotUtils.setState(0, 3, 'south')
             expect(toyRobotUtils.getState().x).toBe(0);
             expect(toyRobotUtils.getState().y).toBe(3);
-            expect(toyRobotUtils.getState().d).toBe(-1);
+            expect(toyRobotUtils.getState().d).toBe('south');
         });
 
     });
 
-    describe('2.When toy is moved', () => {
+    describe('2.When toy robot is moved', () => {
 
         beforeEach(() => {
-            var directions = {
-                'north': { val: 1, axis: 'y' },
-                'south': { val: -1, axis: 'y' },
-                'west': { val: -1, axis: 'x' },
-                'east': { val: 1, axis: 'x' }
-            };
-        })
+            toyRobotUtils.place(0, 0, 'north');
+        });
 
         it('should increment y by 1 if movement is north', () => {
             toyRobotUtils.setState(2, 2, 'north');
@@ -67,8 +62,88 @@ describe('ToyRobotTests', function() {
             }).toThrowError(Error);
         });
 
-    })
+    });
 
+    describe('3. When the toy robot is turned', () => {
 
+        beforeEach(() => {
+            toyRobotUtils.place(0, 0, 'north');
+        });
 
+        it('should change direction when turned', () => {
+            toyRobotUtils.setState(2, 2, 'west');
+            toyRobotUtils.turn('left');
+            expect(toyRobotUtils.getState().d).toBe('south');
+        });
+
+        it('should change direction when turned', () => {
+            toyRobotUtils.setState(2, 2, 'south');
+            toyRobotUtils.turn('right');
+            expect(toyRobotUtils.getState().d).toBe('west');
+        });
+
+    });
+
+    describe('4. When report button is clicked, should render report in DOM', () => {
+
+        afterEach(() => {
+            try {
+                let state = toyRobotUtils.getState();
+                if (toyRobotUtils.checkPositionValidity(state.x, state.y)) {
+                    let string = [];
+                    for (let key in state) {
+                        if (state.hasOwnProperty(key)) {
+                            string.push(state[key]);
+                        }
+                    }
+                    expect(string.join(', ').toUpperCase()).toBe(result);
+                }
+            } catch (error) {
+                throw error;
+            }
+
+        })
+
+        it('Test 1:', () => {
+            toyRobotUtils.place(0, 0, 'north');
+            toyRobotUtils.move();
+            toyRobotUtils.report();
+
+            result = '0, 1, NORTH';
+        });
+
+        it('Test 2:', () => {
+            toyRobotUtils.place(0, 0, 'north');
+            toyRobotUtils.turn('left');
+            toyRobotUtils.report();
+
+            result = '0, 0, WEST';
+        });
+
+        it('Test 3:', () => {
+            toyRobotUtils.place(1, 2, 'east');
+            toyRobotUtils.move();
+            toyRobotUtils.move();
+            toyRobotUtils.turn('left');
+            toyRobotUtils.move();
+            toyRobotUtils.report();
+
+            result = '3, 3, NORTH';
+        });
+
+    });
+
+    describe('5. When movement of toy robot is outside the table bounds', () => {
+
+        it('should throw an error', () => {
+            //ERROR TEST
+            expect(() => {
+                toyRobotUtils.place(2, 2, 'north');
+                toyRobotUtils.move();
+                toyRobotUtils.move();
+                toyRobotUtils.move();
+                toyRobotUtils.report();
+            }).toThrowError();
+        });
+    });
 });
